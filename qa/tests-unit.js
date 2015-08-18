41406c8e3f2393fccfc9ba17d7bf8fe107d2cd35
Teresa Martyny <teresamartyny@gmail.com>
2015-08-18 14:00:31 -0700

    fix spelling on file name to be plural

---
+++ qa/tests-unit.js
@@ -0,0 +1,13 @@
+// require js files to be tested
+  // customJsFile = require('../lib/customJsFile.js');
+  
+// test suites
+
+  // documentation: https://mochajs.org/
+  // 
+  // TDD example:
+  // suite('Describe test coverage', function(){
+  //  test('function name should do xyz', function(){
+  //    expect(put expectation here); 
+  //  });
+  // });
